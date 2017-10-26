ad3866b10e15d88900d7a86457ce7ad2daac351e
Jan Schneider <schneider@zeiss-izm.de>
2017-10-26 16:02:51 +0200

    chore: adjusted assembly version to match nuget package version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("PiWeb MeshModel")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyCompany("Carl Zeiss Innovationszentrum für Messtechnik GmbH")]
 [assembly: AssemblyProduct("PiWeb")]
 [assembly: AssemblyCopyright("Copyright © 2017 Carl Zeiss Innovationszentrum für Messtechnik GmbH")]

f333b20f65129572ef3463ec189b0f433eecfef2
Georg Eckert <georg.eckert@zeiss.com>
2021-11-15 16:38:01 +0100

    chore: Upgrades to .NET 6
    Makes MeshModel compatible with .NET 6 by fixing a [breaking change](https://docs.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/partial-byte-reads-in-streams).

---
+++ src/Tests/AssertingStream.cs
@@ -0,0 +1,79 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2021                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+namespace Zeiss.PiWeb.MeshModel.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using NUnit.Framework;
+    public sealed class AssertingStream : Stream
+    {
+        private readonly byte[] _Original;
+        private readonly MemoryStream _Actual;
+        private readonly List<(long start, int count)> _Diffs = new List<(long start, int count)>();
+        public AssertingStream( byte[] original )
+        {
+            _Original = original;
+            _Actual = new MemoryStream( _Original.Length );
+        }
+        public override void Flush()
+        {
+            _Actual.Flush();
+        }
+        public override int Read( byte[] buffer, int offset, int count )
+        {
+            return _Actual.Read( buffer, offset, count );
+        }
+        public override long Seek( long offset, SeekOrigin origin )
+        {
+            return _Actual.Seek( offset, origin );
+        }
+        public override void SetLength( long value )
+        {
+            _Actual.SetLength( value );
+        }
+        public override void Write( byte[] buffer, int offset, int count )
+        {
+            var actual = buffer.AsSpan( offset, count ).ToArray();
+            var expected = _Original.AsSpan( (int) Position, count ).ToArray();
+            _Actual.Write( buffer, offset, count );
+            if( actual.SequenceEqual( expected ) )
+            {
+                var diffIndex = _Diffs.IndexOf( ( Position - count, count ) );
+				if( diffIndex <= -1 )
+					return;
+
+				TestContext.WriteLine( $"Correct diff at position: {Position - count} - {Position} (#{diffIndex + 1})" );
+				TestContext.WriteLine( $"Corrected: {string.Join( ", ", actual )}" );
+				return;
+            }
+            _Diffs.Add( ( Position - count, count ) );
+			TestContext.WriteLine( $"   i    | Expected | Actual  " );
+			TestContext.WriteLine( $"--------+----------+----------" );
+			const string marker = "<<";
+			for( var i = 0; i < expected.Length; ++i )
+			{
+				var mark = expected[ i ] == actual[ i ] ? string.Empty : marker;
+				TestContext.WriteLine( $"{Position - count + i, 7} | {expected[i], 8} |{actual[i], 8}  {mark}" );
+			}
+        }
+        public override bool CanRead => _Actual.CanRead;
+        public override bool CanSeek => _Actual.CanSeek;
+        public override bool CanWrite => _Actual.CanWrite;
+        public override long Length => _Actual.Length;
+        public override long Position
+        {
+            get => _Actual.Position;
+            set => _Actual.Position = value;
+        }
+    }
+}
--- src/Tests/MeshTest.cs
+++ src/Tests/MeshTest.cs
@@ -16,7 +16,9 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 {
 	#region usings
 
+	using System.Buffers;
 	using System.Diagnostics;
+	using System.Linq;
 	using NUnit.Framework;
 	using Zeiss.PiWeb.MeshModel;
 
@@ -27,16 +29,37 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 	{
 		#region members
 
-		private static readonly string MeshModelTestFile = Path.Combine(
+		private static readonly string TestDataPath = Path.Combine(
 			TestContext.CurrentContext.TestDirectory,
-			"TestData",
-			"test_deviation.meshModel" );
+			"TestData" );
+
+		private static readonly string MeshModelTestFile = Path.Combine( TestDataPath, "test_deviation.meshModel" );
+		private static readonly string MeshesTestFile = Path.Combine( TestDataPath, "Meshes.dat" );
+		private static readonly string EdgesTestFile = Path.Combine( TestDataPath, "Edges.dat" );
+		private static readonly string DeviationTestFile0 = Path.Combine( TestDataPath, "deviation0.dat" );
+		private static readonly string DeviationTestFile1 = Path.Combine( TestDataPath, "deviation1.dat" );
+		private static readonly string DeviationTestFile2 = Path.Combine( TestDataPath, "deviation2.dat" );
+		private static readonly string DeviationTestFile3 = Path.Combine( TestDataPath, "deviation3.dat" );
+		private static readonly string DeviationTestFile4 = Path.Combine( TestDataPath, "deviation4.dat" );
+
+		private static readonly string MetaDataTestFile = Path.Combine( TestDataPath, "Metadata.xml" );
+
+		private static readonly Version TestVersion = new Version( 5, 1, 0, 0 );
 
 		private static readonly string MeshModelReWrittenFile = $"{MeshModelTestFile}.rewritten.meshModel";
+
+		private MeshModelMetadata _Metadata;
 
 		#endregion
 
 		#region methods
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			using var stream = File.OpenRead( MetaDataTestFile );
+			_Metadata = MeshModelMetadata.ReadFrom( stream );
+		}
 
 		internal static Vector3F[] Create4Positions()
 		{
@@ -130,16 +153,122 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 			Assert.AreEqual( positions[ 2 ], mesh.GetTriangle( 0 ).VertexC );
 		}
 
+		[Test, Description( "Given: Metadata file, When: imported, Then: Imported values are correct." )]
+		public void ImportExportMetadataTest()
+		{
+			// ## Given ##
+			var originalInfo = new FileInfo( MetaDataTestFile );
+			var originalBytes = File.ReadAllBytes( originalInfo.FullName );
+
+			// ## When & Then ##
+			using var fsOriginal = originalInfo.OpenRead();
+			using var fsReWritten = new AssertingStream( originalBytes );
+
+			var metadata = MeshModelMetadata.ReadFrom( fsOriginal );
+			metadata.WriteTo( fsReWritten, false );
+
+			Assert.That( originalBytes.Length, Is.EqualTo( fsReWritten.Position ) );
+		}
+
+		[Test, Description( "Given: Meshes file, When: imported, Then: Imported values are correct." )]
+		public void ImportExportMeshesTest()
+		{
+			// ## Given ##
+			var originalInfo = new FileInfo( MeshesTestFile );
+			var originalBytes = File.ReadAllBytes( originalInfo.FullName );
+
+			// ## When & Then ##
+			using var fsOriginal = originalInfo.OpenRead();
+			using var fsReWritten = new AssertingStream( originalBytes );
+			var stopwatch = new Stopwatch();
+
+			stopwatch.Start();
+			var meshes = MeshModelPart.ReadMeshes( fsOriginal, TestVersion );
+			stopwatch.Stop();
+
+			var stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Deserialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			stopwatch.Start();
+			MeshModelPart.SerializeMeshes( fsReWritten, meshes );
+			stopwatch.Stop();
+
+			stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Serialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			Assert.That( originalBytes.Length, Is.EqualTo( fsReWritten.Position ) );
+		}
+
+		[Test, Description( "Given: Edges file, When: imported, Then: Imported values are correct." )]
+		public void ImportExportEdgesTest()
+		{
+			// ## Given ##
+			var originalInfo = new FileInfo( EdgesTestFile );
+			var originalBytes = File.ReadAllBytes( originalInfo.FullName );
+
+			// ## When & Then ##
+			using var fsOriginal = originalInfo.OpenRead();
+			using var fsReWritten = new AssertingStream( originalBytes );
+
+			var stopwatch = new Stopwatch();
+
+			stopwatch.Start();
+			var edges = MeshModelPart.ReadEdges( fsOriginal, TestVersion );
+			stopwatch.Stop();
+			var stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Deserialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			stopwatch.Start();
+			MeshModelPart.SerializeEdges( fsReWritten, edges );
+			stopwatch.Stop();
+
+			stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Serialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			Assert.That( originalBytes.Length, Is.EqualTo( fsReWritten.Position ) );
+		}
+
+		[Test, Description( "Given: Deviation file, When: imported, Then: Imported values are correct." )]
+		[TestCaseSource( nameof( DeviationFileTestCaseProvider ) )]
+		public void ImportExportMeshValueListTest( int meshValueEntry, string deviationFileName, string deviationFilePath )
+		{
+			// ## Given ##
+			var originalInfo = new FileInfo( deviationFilePath );
+			var originalBytes = File.ReadAllBytes( originalInfo.FullName );
+
+			// ## When & Then ##
+			using var fsOriginal = originalInfo.OpenRead();
+			using var fsReWritten = new AssertingStream( originalBytes );
+
+			var stopwatch = new Stopwatch();
+
+			stopwatch.Start();
+			var meshValueList = MeshModelPart.ReadMeshValueList(
+				fsOriginal,
+				TestVersion,
+				_Metadata.MeshValueEntries[ meshValueEntry ] );
+			stopwatch.Stop();
+			var stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Deserialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			stopwatch.Start();
+			MeshModelPart.SerializeMeshValueList( fsReWritten, meshValueList );
+			stopwatch.Stop();
+
+			stopwatchElapsed = stopwatch.Elapsed;
+			TestContext.WriteLine( $"Serialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
+
+			Assert.That( originalBytes.Length, Is.EqualTo( fsReWritten.Position ) );
+		}
+
 		[Test, Description( "Given: Test file, When: imported, Then: Imported values are correct." )]
 		public void ImportTest()
 		{
 			// ..................................................................................... GIVEN
 			using var file = File.OpenRead( MeshModelTestFile );
 
-
 			// ..................................................................................... WHEN
 			var meshModel = MeshModel.Deserialize( file );
-
 
 			// ..................................................................................... THEN
 			Assert.That( meshModel.Metadata.FileVersion == new Version( 5, 1, 0, 0 ) );
@@ -151,6 +280,9 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 			Assert.That( meshModel.Metadata.MeshValueEntries.Length, Is.EqualTo( 5 ) );
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 0 ].Filename, Is.EqualTo( "deviation0.dat" ) );
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 1 ].Filename, Is.EqualTo( "deviation1.dat" ) );
+			Assert.That( meshModel.Metadata.MeshValueEntries[ 2 ].Filename, Is.EqualTo( "deviation2.dat" ) );
+			Assert.That( meshModel.Metadata.MeshValueEntries[ 3 ].Filename, Is.EqualTo( "deviation3.dat" ) );
+			Assert.That( meshModel.Metadata.MeshValueEntries[ 4 ].Filename, Is.EqualTo( "deviation4.dat" ) );
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 0 ].ColorScale.Name, Is.EqualTo( "SimpleDynamic" ) );
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 0 ].ColorScale.Interpolation,
 				Is.EqualTo( ColorScaleInterpolation.HSV ) );
@@ -162,6 +294,13 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 0 ].ColorScale.Entries[ 0 ].LeftColor,
 				Is.EqualTo( Color.FromRgb( 67, 122, 180 ) ) );
 			Assert.That( meshModel.Metadata.MeshValueEntries[ 0 ].ColorScale.Entries[ 0 ].Value, Is.LessThan( -2f ) );
+			Assert.That( meshModel.Parts[ 0 ].Edges.Length, Is.EqualTo( 4850 ) );
+
+			var triangleSum = meshModel.Parts
+				.Select( p => p )
+				.SelectMany( p => p.Meshes )
+				.Sum( m => m.TriangleCount );
+			Assert.That( triangleSum, Is.EqualTo( 17326 ) );
 		}
 
 
@@ -172,50 +311,60 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 			var originalInfo = new FileInfo( MeshModelTestFile );
 			var reWrittenInfo = new FileInfo( MeshModelReWrittenFile );
 
-
 			// ..................................................................................... WHEN
 			using( var fsOriginal = originalInfo.OpenRead() )
+			using( var fsReWritten = reWrittenInfo.Create() )
 			{
-				using( var fsReWritten = reWrittenInfo.OpenWrite() )
-				{
-					var stopwatch = new Stopwatch();
+				var stopwatch = new Stopwatch();
 
-					stopwatch.Start();
-					var meshModel = MeshModel.Deserialize( fsOriginal );
-					stopwatch.Stop();
-					var stopwatchElapsed = stopwatch.Elapsed;
-					TestContext.Out.WriteLine($"Deserialization: {Convert.ToInt32(stopwatchElapsed.TotalMilliseconds)} ms");
+				stopwatch.Start();
+				var meshModel = MeshModel.Deserialize( fsOriginal );
+				stopwatch.Stop();
+				var stopwatchElapsed = stopwatch.Elapsed;
+				TestContext.WriteLine( $"Deserialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
 
-					stopwatch.Start();
-					meshModel.Serialize( fsReWritten );
-					stopwatch.Stop();
-					stopwatchElapsed = stopwatch.Elapsed;
-					TestContext.Out.WriteLine($"Serialization: {Convert.ToInt32(stopwatchElapsed.TotalMilliseconds)} ms");
-				}
+				stopwatch.Start();
+				meshModel.Serialize( fsReWritten );
+				stopwatch.Stop();
+				stopwatchElapsed = stopwatch.Elapsed;
+				TestContext.WriteLine( $"Serialization: {Convert.ToInt32( stopwatchElapsed.TotalMilliseconds )} ms" );
 			}
 
 			reWrittenInfo.Refresh();
 
-
-			// ..................................................................................... THEN
-			Assert.That( reWrittenInfo.Length, Is.EqualTo( originalInfo.Length ) );
+			var totalBytes = (int)originalInfo.Length;
+			const int arrayLength = 1024;
 
 			var byteWiseEqual = true;
-			using( var fsOriginal = originalInfo.OpenRead() )
-			{
-				using( var fsReWritten = reWrittenInfo.OpenRead() )
-				{
-					var originalBuffer = new byte[ originalInfo.Length ];
-					var reWrittenBuffer = new byte[ originalInfo.Length ];
-					fsOriginal.Read( originalBuffer, 0, originalBuffer.Length );
-					fsReWritten.Read( reWrittenBuffer, 0, reWrittenBuffer.Length );
 
-					for( var i = 0; i < originalBuffer.Length; ++i )
+			var current = 0;
+			var originalBuffer = ArrayPool<byte>.Shared.Rent( arrayLength );
+			var reWrittenBuffer = ArrayPool<byte>.Shared.Rent( arrayLength );
+
+			using( var fsOriginal = originalInfo.OpenRead() )
+			using( var fsReWritten = reWrittenInfo.OpenRead() )
+			{
+				while( true )
+				{
+					var count = Math.Min( arrayLength, totalBytes - current );
+					if( count <= 0 )
+						break;
+
+					var readByteCountOriginal = fsOriginal.Read( originalBuffer, 0, count );
+					var readByteCountReWritten = fsReWritten.Read( reWrittenBuffer, 0, count );
+
+					var commonReadLength = Math.Min( readByteCountOriginal, readByteCountReWritten );
+					for( var i = 0; i < count; ++i )
 					{
 						byteWiseEqual &= originalBuffer[ i ] == reWrittenBuffer[ i ];
 						if( !byteWiseEqual )
 							break;
 					}
+
+					if( !byteWiseEqual )
+						break;
+
+					current += commonReadLength;
 				}
 			}
 
@@ -248,6 +397,15 @@ namespace Zeiss.PiWeb.MeshModel.Tests
 			Assert.That( meshModel.Metadata.MeshValueEntries.Length, Is.EqualTo( 0 ) );
 			Assert.That( meshModel.Parts[ 0 ].Meshes.Length, Is.EqualTo( 1 ) );
 			Assert.That( meshModel.Parts[ 0 ].Edges.Length, Is.EqualTo( 1 ) );
+		}
+
+		private static IEnumerable<TestCaseData> DeviationFileTestCaseProvider()
+		{
+			yield return new TestCaseData( 0, "deviation0.dat", DeviationTestFile0 ).SetName( "deviation0.dat" );
+			yield return new TestCaseData( 1, "deviation1.dat", DeviationTestFile1 ).SetName( "deviation1.dat" );
+			yield return new TestCaseData( 2, "deviation2.dat", DeviationTestFile2 ).SetName( "deviation2.dat" );
+			yield return new TestCaseData( 3, "deviation3.dat", DeviationTestFile3 ).SetName( "deviation3.dat" );
+			yield return new TestCaseData( 4, "deviation4.dat", DeviationTestFile4 ).SetName( "deviation4.dat" );
 		}
 
 		private static ColorScale CreateGreyscale()
